f8c412ee00d8f44f00e7b83dca665c294daa14a5
FakeJordansConnoisseur <114087644+FakeJordansConnoisseur@users.noreply.github.com>
2024-01-10 14:50:41 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-    //CSS taken from JS2 project, a little code taken from dice game project
-    d1 = 0;
-    d2 = 0;
-    d3 = 0;
-       function roll(){
-           d1 = parseInt(Math.random() * 6);
-           d2 = parseInt(Math.random() * 6);
-           d3 = parseInt(Math.random() * 6);
-        //    write()
-       }
-    //    function write(){
-    //        why = document.getElementById("p");
-    //        text = document.createTextNode("Dice 1 = "+d1);
-    //        why.appendChild(text);
-    //        text = document.createTextNode("  Dice 2 = "+d2);
-    //        why.appendChild(text);
-    //        text = document.createTextNode("  Total = "+(d1+d2));
-    //        why.appendChild(text);
-    //    }
+//CSS taken from JS2 project, a little code taken from dice game project
+
+count = 0;
+times = 0;
+dlist = [];
+sumlist = [];
+d1 = 0;
+d2 = 0;
+d3 = 0;
+summation = 0;
+
+
+
+function roll(){
+   count = document.getElementById("count");
+   times = document.getElementById("times");
+   for (i = 0; times > i; i++){
+   d1 = parseInt(Math.random() * 6)+1;
+   d2 = parseInt(Math.random() * 6)+1;
+   d3 = parseInt(Math.random() * 6)+1;
+   dlist.push()
+   summation = summation+d1+d2+d3;
+   }
+   calculateStats();
+   }
+
+function calclateStats(){
+   
+   document.getElementById("i1")= 
+}
+
+
+// function timesChange(){
+
+// }
+
+// function amountChange(){
+   
+// }
